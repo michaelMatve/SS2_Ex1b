a284726431daf960367c8a24755bbba8201ca566
michael <michael.matve@gmail.com>
2022-03-14 11:50:19 +0200

    with notes

--- mat.cpp
+++ mat.cpp
@@ -5,9 +5,25 @@
 #include <vector>
 
 namespace ariel{
+    /**
+     * @brief 
+     * this function get the number of rows , colunms , simbol1, simbol2
+     * and the fanction creat a mat in the size rows*colunms with the two simbol
+     * we creating a matrix by vectors that will represent our mat.
+     * then we fill the matrix with the simbpls like we ask by runing from the 
+     * boundris to the center.
+     * 
+     * @param weight : number of colums
+     * @param length : number of rows
+     * @param firest_simbol : the firest simbol
+     * @param second_simbol : the second simbol
+     * @return std::string 
+     */
     std::string mat(int weight, int length, char firest_simbol, char second_simbol){
+        // the boundris for the chars we can get 
         const int minc = 33;
         const int maxc = 126; 
+
         if((firest_simbol<minc)||(firest_simbol>maxc))
         {
             throw std::invalid_argument("bad input for simbol A");
@@ -18,10 +34,13 @@ namespace ariel{
             throw std::invalid_argument("bad input for simbol B");
         }
 
+        //check the number is positive
         if(length<=0 || weight<=0)
         {
              throw std::invalid_argument("bad input cant get negetive");
         }
+
+        //check the number of row and colums is odd
         if(length%2 == 0)
         {
             throw std::invalid_argument("bad input can get only odd");
@@ -32,6 +51,7 @@ namespace ariel{
             throw std::invalid_argument("bad input can get only odd");
         }
         
+        // check that the simbol is not ' ' or '    ' or \n or \t
         if(firest_simbol == ' ' || firest_simbol == '\n' || firest_simbol == '\t' || firest_simbol == '\r')
         {
             throw std::invalid_argument("bad input for simbol A");
@@ -42,11 +62,15 @@ namespace ariel{
             throw std::invalid_argument("bad input for simbol B");
         }
         
+        //defind the matrix
         std::vector<std::vector<char>>matrix(length,std::vector<char>(weight));
 
+        // use for the the simbol we work on 
         char simbol = firest_simbol;
+        
         for(int count=0; (count<(length+1)/2) && (count<(weight+1)/2); count++)
         {
+            // every time we check if count is odd if yes we use second simbol if not use firest simbol
             if(count%2 == 0)
             {
                 simbol = firest_simbol;
@@ -55,7 +79,7 @@ namespace ariel{
             {
                 simbol = second_simbol;
             }
-            
+            // fill the matrix 
             for(int coulm=count; coulm<weight-count ; coulm++)
             {
                 matrix[count][coulm] = simbol;
@@ -76,7 +100,9 @@ namespace ariel{
                 matrix[row][weight-1-count] = simbol;
             }
         }
+
         std::string st;
+        //copy the matrix to string
         for(int row=0 ; row < length ; row++)
         {
             for(int coulm = 0 ; coulm < weight; coulm++)
@@ -85,6 +111,7 @@ namespace ariel{
             }
             st += "\n";
         }
+        
         return st;
     }
 }
